0979d6fdddaec19e745459c455f736e1c4bf5af2
Osiris Gonzalez <tompsom98@gmail.com>
2022-09-23 22:35:31 -0500

    Add files via upload

---
+++ apoco chi/Boostrap act 7/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class ="container">
+        <section class="cont">
+                <div class="row cont justify-content-md-center aling align-items-center">
+                <div class="col-md-6 col-sm-12">
+                    <form action="">
+                        <h1 class="text-center">
+                            Inicia Sesion
+                        </h1>
+                        <div class="mb-3">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">O</span>
+                                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">O</span>
+                                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        
+                        <button type="submit" class="btn btn-dark col-sm-12" href="ActividaBosstrap/product/productos.php">Acceder</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
